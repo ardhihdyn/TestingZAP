85f037b8ddbf779c2bb023e9614e1da57cfc4da1
Angga <muhammada@fresh-forces.com>
2023-08-24 14:45:30 +0700

    testcommit

---
+++ zap-eli.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set your target URL
+TARGET_URL="https://your-website.com"
+
+# Start ZAP in daemon mode
+zap.sh -daemon -port 8080
+
+# Perform a spider scan
+zap-cli --zap-url http://localhost:8080 -p 8080 spider $TARGET_URL
+
+# Wait for the spider to finish (you may need to adjust this duration)
+sleep 30
+
+# Perform an active scan
+zap-cli --zap-url http://localhost:8080 -p 8080 active-scan $TARGET_URL
+
+# Generate a report (HTML or other formats)
+zap-cli --zap-url http://localhost:8080 -p 8080 report -o /path/to/report.html -f html
+
+# Shut down ZAP
+zap-cli --zap-url http://localhost:8080 -p 8080 shutdown
