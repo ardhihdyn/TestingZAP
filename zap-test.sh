cf50d5f0b865ee5b447a8febfd22fec343192eb6
muhamadangga <57985588+muhamadangga@users.noreply.github.com>
2023-08-25 15:21:20 +0700

    Update zap-test.sh

--- zap-test.sh
+++ zap-test.sh
@@ -3,11 +3,9 @@
 # Set your target URL
 TARGET_URL="http://brainportstg.wpengine.com/"
 
-
-
 # Start ZAP in daemon mode
 # /snap/bin/zaproxy -daemon -port 8080
-zap.sh -daemon -port 8080
+sudo ./zap.sh -daemon -port 8080
 
 # Perform a spider scan
 zap-cli --zap-url http://localhost:9090 -p 8080 spider $TARGET_URL
