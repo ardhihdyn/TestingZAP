9fa2393ecbf6a126c3d26d6e98caa83a4a09d7a1
muhamadangga <57985588+muhamadangga@users.noreply.github.com>
2023-08-25 15:00:51 +0700

    Update zap-test.sh

--- zap-test.sh
+++ zap-test.sh
@@ -4,19 +4,19 @@
 TARGET_URL="http://brainportstg.wpengine.com/"
 
 # Start ZAP in daemon mode
-/snap/bin/zaproxy -daemon -port 8084
+/snap/bin/zaproxy -daemon -port 9090
 
 # Perform a spider scan
-zap-cli --zap-url http://localhost:8084 -p 8084 spider $TARGET_URL
+zap-cli --zap-url http://localhost:9090 -p 9090 spider $TARGET_URL
 
 # Wait for the spider to finish (you may need to adjust this duration)
 sleep 30
 
 # Perform an active scan
-zap-cli --zap-url http://localhost:8084 -p 8084 active-scan $TARGET_URL
+zap-cli --zap-url http://localhost:9090 -p 9090 active-scan $TARGET_URL
 
 # Generate a report (HTML or other formats)
-zap-cli --zap-url http://localhost:8084 -p 8084 report -o /path/to/report.html -f html
+zap-cli --zap-url http://localhost:9090 -p 9090 report -o /path/to/report.html -f html
 
 # Shut down ZAP
-zap-cli --zap-url http://localhost:8084 -p 8084 shutdown
+zap-cli --zap-url http://localhost:9090 -p 9090 shutdown
